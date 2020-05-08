965ebabdacf07f3824f5bc21e4aba472ea802a9e
shriraam <bshriraamreddy11@gmail.com>
2020-05-08 12:06:36 +0530

    exception handling

--- src/main/java/evalvisitor.java
+++ src/main/java/evalvisitor.java
@@ -1,12 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
+
 import java.lang.Math;
 
 public class evalvisitor extends calcBaseVisitor<Integer> {
-    Map<String, Integer> memory = new HashMap<String, Integer>();
-   
 
-        @Override
+    @Override
     public Integer visitPrintExpr(calcParser.PrintExprContext ctx) {
         Integer value = visit(ctx.expr()); // evaluate the expr child
         System.out.println(value); // print the result
@@ -14,14 +11,15 @@ public class evalvisitor extends calcBaseVisitor<Integer> {
     }
     /** INT */
     @Override
-    public Integer visitInt(calcParser.IntContext ctx) {
+    public Integer visitInt(calcParser.IntContext ctx)
+    {
         return Integer.valueOf(ctx.INT().getText());
     }
     /** ID */
-    @Override public Integer visitPower(calcParser.PowerContext ctx) {
+    @Override
+    public Integer visitPower(calcParser.PowerContext ctx) {
         double left = (double)visit(ctx.expr(0)); // get value of left subexpression
         double right = (double)visit(ctx.expr(1)); // get value of right subexpression
-
         return (int)Math.pow(left,right);
     }
     /** expr op=('*'|'/') expr */
--- src/main/java/read.java
+++ src/main/java/read.java
@@ -1,27 +1,43 @@
 
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ConsoleErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.tree.*;
 
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 public class read {
     public static void main(String[] args) throws Exception {
-
-        Scanner s = new Scanner(System.in);
-        String str=s.nextLine();
+        try{
+                Scanner s = new Scanner(System.in);
+                String str=s.nextLine();
 //        String file="input";
 //
-        calcLexer lexer = new calcLexer(CharStreams.fromString(str));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        calcParser parser = new calcParser(tokens);
-        ParseTree tree = parser.stat(); // begin parsing at init rule
-        evalvisitor visitor = new evalvisitor();
-        visitor.visit(tree);
+                calcLexer lexer = new calcLexer(CharStreams.fromString(str));
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                calcParser parser = new calcParser(tokens);
+                ParseTree tree = parser.stat(); // begin parsing at init rule
+                evalvisitor visitor = new evalvisitor();
+                visitor.visit(tree);
+                //lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
+                //parser.removeErrorListeners();
+                System.out.println(tree.toStringTree(parser));
+        }
+        catch(NullPointerException e){
+            System.out.print("input doesnt match or incomplete expression");
+        }
+        catch (InputMismatchException e ){
+            System.out.print("input doesnt match or incomplete expression");
+        }
+        catch(RecognitionException e){
+            System.out.print("input doesnt match or incomplete expression");
+        }
 
-        System.out.println(tree.toStringTree(parser));
     }
 
 }
